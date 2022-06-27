41927940d8e78599e8f0aeef311c1cc93009c750
Nii <bnstephens001@gmail.com>
2022-06-27 03:16:59 +0000

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const init = () => {
+    const inputForm = document.getElementById('form');
+
+const init = () => {const ids = []
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            ids.push(element.id)   
+        });
+       })
+       inputForm.addEventListener('submit', (event) => {
+           event.preventDefault()})
+
+       const input = document.querySelector('input#searchByID');
+        let userSearch = input.value
+
+        if (ids.includes(parseInt(userSearch))){
+           fetch(`http://localhost:3000/movies/${userSearch}`)
+           .then(response => response.json())
+           .then(data => {
+           const title = document.querySelector('section#movieDetails h4');
+           const summary = document.querySelector('section#movieDetails p');
+           title.innerText = data.title;
+           summary.innerText = data.summary;})
+       }else{
+           alert(`${userSearch} is not a valid id`)
+       }
+ 
   
+}
 }
 
 document.addEventListener('DOMContentLoaded', init);
